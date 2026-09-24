64b21f200a926f794698e7d88e3386bb1dca93f9
Seyyed Mehdi Mousavi <mehdimasiha396@gmail.com>
2026-09-24 19:28:36 +0330

    Add files via upload

---
+++ higherFunction.js
@@ -0,0 +1,27 @@
+function add(num1 , num2) {
+    return num1 + num2
+    
+};
+
+function menha(num1 , num2) {
+    return num1 - num2
+    
+};
+
+function zarb(num1 , num2) {
+    return num1 * num2 
+    
+};
+
+function taghsim(num1 , num2) {
+
+    return num1/num2
+    
+};
+
+function  calculator(num1,num2,op) {
+    return op(num1 , num2)
+    
+};
+
+calculator(5 , 6 , taghsim);
